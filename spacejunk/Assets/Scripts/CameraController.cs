d254b14f8000b3693af674ac486ac36d012266ca
Bobby Brace <sedarmor@gmail.com>
2018-01-13 23:53:20 -0600

    set camera to follow player on scroll but not vertical

---
+++ spacejunk/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float scrollRate;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.MovePosition(new Vector2(rb.position.x + scrollRate, 0f));
+    }
+}
