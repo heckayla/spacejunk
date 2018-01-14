04f148752bacd6a916b986a79af086c740823e1c
Bobby Brace <sedarmor@gmail.com>
2018-01-13 20:45:38 -0600

    added credits menu and controls for navigating with controller

--- spacejunk/Assets/Scripts/MainMenu.cs
+++ spacejunk/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour {
 
-    
+    public GameObject mainMenu;
+    public GameObject credits;
+    public Button playButton;
+    public Button menuButton;
+
+    private void Start()
+    {
+        
+    }
+
     public void LoadGarbageLevel()
     {
         SceneManager.LoadScene("Junk_Collect");
+    }
+
+    public void LoadCredits()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+        playButton.Select();
+        EventSystem.current.SetSelectedGameObject(menuButton.gameObject);
+    }
+
+    public void LoadMainMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(playButton.gameObject);
+
     }
 }
