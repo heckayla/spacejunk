307d2bebb0d857cc5d03b3ff74e6f07a76c8f6ae
Michael <bigge019@umn.edu>
2018-01-13 18:57:55 -0600

    made the highlighting of garbage items work!

--- spacejunk/Assets/Scripts/Pickup_Logic.cs
+++ spacejunk/Assets/Scripts/Pickup_Logic.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Pickup_Logic : MonoBehaviour
 {
 
+    public string gbName = "fire_ext";
+    SpriteRenderer spr_rend; //the SpriteRenderer for this piece of garbage. Holds its current sprite
+    Sprite spr_orig; //the initial sprite of this piece of garbage
+    Sprite spr_hl; //the highlighted sprite for this piece of garbage
+
     // Use this for initialization
     void Start()
-    {
-
+    { //loads all the necessary sprites for the garbage. Only two of them tbh
+        spr_rend = this.gameObject.GetComponent<SpriteRenderer>();
+        spr_orig = spr_rend.sprite;
+        spr_hl = Resources.Load<Sprite>("Garbage/" + gbName + "_hl");
+        Debug.Log(gbName);
+        Debug.Log(spr_hl);
     }
 
     // Update is called once per frame
@@ -22,10 +31,12 @@ public class Pickup_Logic : MonoBehaviour
         if (collision.gameObject.tag == "P1")
         {
             Trash_Scene_Controller.instance.setPickupP1(true, this.gameObject);
+            spr_rend.sprite = spr_hl; //highlights the garbage
         }
         else if (collision.gameObject.tag == "P2")
         {
             Trash_Scene_Controller.instance.setPickupP2(true, this.gameObject);
+            spr_rend.sprite = spr_hl; //highlights the garbage
         }
     }
 
@@ -39,5 +50,6 @@ public class Pickup_Logic : MonoBehaviour
         {
             Trash_Scene_Controller.instance.setPickupP2(false, this.gameObject);
         }
+        spr_rend.sprite = spr_orig; //de-highlights the garbage
     }
 }
