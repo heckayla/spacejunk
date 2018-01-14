743b73d3e73c31973bfb5e13a5b56a998908bdc5
Bobby Brace <sedarmor@gmail.com>
2018-01-14 12:54:03 -0600

    added raccoon animations

---
+++ spacejunk/Assets/Scripts/JunkController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkController : MonoBehaviour {
+
+    public Sprite[] JunkSprites;
+
+    public SpaceJunk[] SpaceJunks;
+	// Use this for initialization
+	void Start () {
+		foreach (SpaceJunk junk in SpaceJunks)
+        {
+            SpriteRenderer sr = junk.GetComponent<SpriteRenderer>();
+            sr.sprite = JunkSprites[new System.Random().Next(0, JunkSprites.Length)];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- spacejunk/Assets/Scripts/SpaceJunk.cs
+++ spacejunk/Assets/Scripts/SpaceJunk.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceJunk : MonoBehaviour {
-
-    public Sprite[] junkSprites;
-
-    private SpriteRenderer sr;
+    
+    
 
 	// Use this for initialization
 	void Start () {
-        sr = GetComponent<SpriteRenderer>();
-        sr.sprite = junkSprites[new System.Random().Next(0, junkSprites.Length)];
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
 }
